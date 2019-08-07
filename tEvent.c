3290be486182c7dec449d25a64a506662bcf7544
Xinkerr <634326056@qq.com>
2019-08-07 09:18:35 +0800

    修改

--- tEvent.c
+++ tEvent.c
@@ -1,8 +1,29 @@
-/****************************************************
+/***************************************************************************
+Copyright (c) 2019, Xinkerr
+
+This file is part of tiny event driver.
+
+tiny event driver is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+tiny event driver is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with tiny event driver.  If not, see <https://www.gnu.org/licenses/>.
+	
+LICENSE: LGPL V3.0
+see: http://www.gnu.org/licenses/lgpl-3.0.html
+ 
 Date£∫  2019/8/5
 Author: ‡ç‘M
+Version:1.0
 Github: https://github.com/Xinkerr/tiny-event-driver
-****************************************************/
+****************************************************************************/
 
 #include "tEvent.h"
 
