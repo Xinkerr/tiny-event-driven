7ecf56a029c6338696e62ad53e1e32d9a70bd2f4
Xinkerr <634326056@qq.com>
2021-01-09 21:26:49 +0800

    暂时删除example

--- example/STM32_example/kernel/systick.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SYSTICK_H__
-#define __SYSTICK_H__
-#include <stdint.h>
-void systick_timer_init(uint32_t tick_us, void (*sys_callback)(void));
-void systick_timer_start(void);
-void systick_timer_stop(void);
-
-#endif
-
